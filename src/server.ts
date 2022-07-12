67f3af24cf438b7b42393ae205419148b3c3fa9b
Parsa Attari <parsa@portalhq.io>
2022-07-12 15:53:37 -0400

    Normal signing of transaction

--- src/server.ts
+++ src/server.ts
@@ -91,13 +91,15 @@ app.post('/webhook', async (req, res) => {
       const { transaction, address } = req.body
       
       console.log(`signTransaction request from address: ${address}`)
-      // const signingKey = await walletService.getSigningKey(address)
-      // const signature = signingKey.signDigest(
-      //   keccak256(serialize(<UnsignedTransaction>transaction))
-      // )
+      const signingKey = await walletService.getSigningKey(address)
+      const signature = signingKey.signDigest(
+        keccak256(serialize(<UnsignedTransaction>transaction))
+      )
       // below signature is for transaction sending 1 wei from address 0xce6AC8aa39037e380dA92251d0D17e0B127e0b26
-      const signature = "0xf85f8080826000949d781b46af5ea12a74d88b0ecebdb9695899599f01801ba0551d4fa300471e536d50b794c9dcc1ad474e1de778e4067d8e61d8383bb8a623a020e0f1bd757b78545ac27ee91b45107897295f7e1b9f40e919a9bdcb778d34ce"
-      console.log(`Responding with Signature: ${signature}`)
+      // const signature = "0xf85f8080826000949d781b46af5ea12a74d88b0ecebdb9695899599f01801ba0551d4fa300471e536d50b794c9dcc1ad474e1de778e4067d8e61d8383bb8a623a020e0f1bd757b78545ac27ee91b45107897295f7e1b9f40e919a9bdcb778d34ce"
+      console.log(`Responding with Signature: ${signature} for address ${address}`)
+      console.log(signature);
+      
       res.status(200).send(signature)
     } else if (req.body?.method === 'signTypedData') {
       const { data, address } = req.body

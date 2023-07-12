195c8d608fc8e12a8de8ff530988773701d96117
kelsonic <kgadams93@gmail.com>
2023-07-12 19:46:39 +0200

    Update seedDemo

--- src/seed-demo.ts
+++ src/seed-demo.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client"
-import PortalApi from "./libs/PortalApi"
-import HotWalletService from "./services/HotWalletService"
-import MobileService from "./services/MobileService"
-import { CUSTODIAN_API_KEY, WEBHOOK_URL, WEBHOOK_SECRET } from "./config"
+import { PrismaClient } from '@prisma/client'
+import PortalApi from './libs/PortalApi'
+import HotWalletService from './services/HotWalletService'
+import MobileService from './services/MobileService'
+import { CUSTODIAN_API_KEY, WEBHOOK_URL, WEBHOOK_SECRET } from './config'
 
 async function seedDemo() {
   if (!CUSTODIAN_API_KEY || CUSTODIAN_API_KEY === 'test-api-key') {
@@ -17,12 +17,12 @@ async function seedDemo() {
 
   const prisma = new PrismaClient()
 
-  const hotWalletService = new HotWalletService(prisma, "", "")
+  const hotWalletService = new HotWalletService(prisma, '', '')
   const mobileService = new MobileService(prisma, hotWalletService)
   const portalApi = new PortalApi(CUSTODIAN_API_KEY)
 
   // demo user & demo custodian id
-  await mobileService.createUser("demo")
+  await mobileService.createUser('demo', false)
   await portalApi.registerWebhook(WEBHOOK_URL, WEBHOOK_SECRET) // demo custodian id
 }
 

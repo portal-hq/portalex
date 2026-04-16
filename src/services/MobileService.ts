10615fef76eef683bc8f77e6ca69d04b51fe4c24
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2026-04-16 16:37:40 -0300

    Fix bug where approves/revokes do not parse signature (#67)
    * Fix bug where approves/revokes do not parse signature
    
    * Fix add V2 to solana
    
    * Add delegated tx
    
    * Change delegated tx to delegated transfer

--- src/services/MobileService.ts
+++ src/services/MobileService.ts
@@ -930,7 +930,17 @@ class MobileService {
       return Array.from(seen)
     }
 
-    if (type === 'SOLANA_TX_V1' && data && Array.isArray(data.rawEvents)) {
+    if (
+      [
+        'SOLANA_REVOKE_V2',
+        'SOLANA_DELEGATED_TRANSFER_V2',
+        'SOLANA_APPROVE_V2',
+        'SOLANA_TX_V2',
+        'SOLANA_TX_V1',
+        'SOLANA_APPROVE_V1',
+        'SOLANA_REVOKE_V1'
+      ].includes(type) && data && Array.isArray(data.rawEvents)
+    ) {
       for (const ev of data.rawEvents) {
         const sig = ev?.signature as string
         if (typeof sig === 'string' && sig) seen.add(sig)
